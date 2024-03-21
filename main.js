da1f34f566ac75d9784e42a90d0e87f77ae0f5c3
Ylenia <05ylenia@gmail.com>
2024-03-21 17:45:01 +0100

    format document

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([-43.883333, 170.516667], 13);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
