5222b25469b1f004def99bf3ee2729d93474cb11
Ylenia <05ylenia@gmail.com>
2024-03-21 17:44:28 +0100

    Script in main eingefügt

---
+++ main.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([-43.883333, 170.516667], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([-43.883333, 170.516667]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am in a lake.").openPopup();
