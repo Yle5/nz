101be10791e3da6f935cfff356826deb137830d2
Ylenia <05ylenia@gmail.com>
2024-03-21 17:49:48 +0100

    Kleine Änderung bei der Map

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-43.883333, 170.516667], 13);
+var map = L.map('map').setView([-43.883333, 170.516667], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
