ef0a07ef56ff7fd4105a78b9a32f2d91225ee51f
Ylenia <05ylenia@gmail.com>
2024-04-11 15:55:51 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([-43.883333, 170.516667], 11);
+let map = L.map('map').setView([-43.883333, 170.516667], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-43.883333, 170.516667]).addTo(map);
+let marker = L.marker([-43.883333, 170.516667]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am in a lake.").openPopup();
