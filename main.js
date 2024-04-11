5b017a1f4c09111cd93fc72186edc8d390cf18d9
Ylenia <05ylenia@gmail.com>
2024-04-11 16:05:41 +0200

    Variation lat, lng und zoom verwendet

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-let map = L.map('map').setView([-43.883333, 170.516667], 11);
+let lat = -43.883333;
+let lng = 170.516667;
+let zoom = 11;
+
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([-43.883333, 170.516667]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am in a lake.").openPopup();
